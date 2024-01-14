0d5a7b185e3ff8895a241440c0f4fc73e7b5ec23
TechRiderWR <techrider@wingriders.com>
2024-01-14 16:10:13 +0100

    Use ZOD for parsing config (#7)
    - Add pino and zod as dependencies
    - Change NETWORK to NETWORK_NAME
    
    Not sure why this PR for validation of hex strings was closed: https://github.com/colinhacks/zod/pull/892, but I think using regexp is not a big deal for us

--- scripts/src/config.ts
+++ scripts/src/config.ts
@@ -1,69 +1,40 @@
 import {Asset, NetworkName} from '@wingriders/cab/types'
+import z from 'zod'
+import pino from 'pino'
 import dotenv from 'dotenv'
 
-const env = dotenv.config(process.env.DOTENV_CONFIG_PATH ? {path: process.env.DOTENV_CONFIG_PATH} : {})
+dotenv.config(process.env.DOTENV_CONFIG_PATH ? {path: process.env.DOTENV_CONFIG_PATH} : {})
 
-const parseString = (data: unknown): string => {
-  if (typeof data !== 'string') {
-    throw new Error('Not a string')
-  }
-  return data
+const envSchema = z.object({
+  NETWORK_NAME: z.nativeEnum(NetworkName),
+  BLOCKCHAIN_EXPLORER_URL: z.string().url(),
+  AGGREGATOR_URL: z.string().url(),
+  API_SERVER_URL: z.string().url(),
+  GOVERNANCE_VOTING_PROPOSALS_WALLET: z.string(),
+  GOVERNANCE_VOTING_PROPOSALS_ACCOUNT_INDEX: z.coerce.number().gte(0),
+  GOVERNANCE_TOKEN_POLICY_ID: z
+    .string()
+    .regex(/^[a-fA-F0-9]+$/)
+    .length(56),
+  GOVERNANCE_TOKEN_ASSET_NAME: z.string().regex(/^[a-fA-F0-9]+$/),
+})
+
+const result = envSchema.safeParse(process.env)
+
+if (!result.success) {
+  // If parsing the env variables fails, log the error and exit with code 1
+  // Here it's not possible to use the logger from ./logger.ts because that
+  // requires a valid config
+  pino({name: 'governance'}).error(
+    {issues: result.error.issues},
+    `Error while parsing the following env variables: ${result.error.issues
+      .map((issue) => issue.path.join('.'))
+      .join(', ')}`
+  )
+  process.exit(1)
 }
 
-export const parseNonEmptyString = (data: unknown): string => {
-  const str = parseString(data)
-  if (str.length === 0) {
-    throw new Error('Empty string')
-  }
-  return str
-}
-
-const parseNetworkString = (data: unknown): NetworkName => {
-  const network = parseNonEmptyString(data) as NetworkName
-  if (!Object.values(NetworkName).includes(network)) {
-    throw new Error('Not a valid network parameter')
-  }
-  return network
-}
-
-export const parsePositiveIntString = (data: unknown): number => {
-  const str = parseString(data)
-  const num = parseInt(str, 10)
-  if (!Number.isSafeInteger(num) || num < 0) {
-    throw new Error('Not a positive integer')
-  }
-  return num
-}
-
-export function parseEnv<T>(key: string, parser?: (data: unknown) => T): T {
-  if (parser) {
-    try {
-      return parser(process.env[key])
-    } catch (err) {
-      console.error(`Invalid environment variable ${key}`, process.env[key])
-      throw err
-    }
-  } else {
-    return process.env[key] as unknown as T
-  }
-}
-
-export const config = {
-  NETWORK_NAME: parseEnv('NETWORK', parseNetworkString) as NetworkName,
-  BLOCKCHAIN_EXPLORER_URL: parseEnv('BLOCKCHAIN_EXPLORER_URL', parseNonEmptyString),
-  AGGREGATOR_URL: parseEnv('AGGREGATOR_URL', parseNonEmptyString),
-  API_SERVER_URL: parseEnv('API_SERVER_URL', parseNonEmptyString),
-  GOVERNANCE_VOTING_PROPOSALS_WALLET: parseEnv(
-    'GOVERNANCE_VOTING_PROPOSALS_WALLET',
-    parseNonEmptyString
-  ),
-  GOVERNANCE_VOTING_PROPOSALS_ACCOUNT_INDEX: parseEnv(
-    'GOVERNANCE_VOTING_PROPOSALS_ACCOUNT_INDEX',
-    parsePositiveIntString
-  ),
-  GOVERNANCE_TOKEN_POLICY_ID: parseEnv('GOVERNANCE_TOKEN_POLICY_ID', parseNonEmptyString),
-  GOVERNANCE_TOKEN_ASSET_NAME: parseEnv('GOVERNANCE_TOKEN_ASSET_NAME', parseNonEmptyString),
-}
+export const config = result.data
 
 export const governanceToken: Asset = {
   policyId: config.GOVERNANCE_TOKEN_POLICY_ID,
